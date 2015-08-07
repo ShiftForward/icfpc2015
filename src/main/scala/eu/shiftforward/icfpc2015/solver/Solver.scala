1fc853267cb0924696076a55ade426995af10f8a
Rui Gonçalves <ruippeixotog@gmail.com>
2015-08-07 21:42:04 +0100

    Update phrases of power list and add some stats

--- src/main/scala/eu/shiftforward/icfpc2015/solver/Solver.scala
+++ src/main/scala/eu/shiftforward/icfpc2015/solver/Solver.scala
@@ -7,13 +7,20 @@ trait Solver {
   def play(initialState: GameState): Seq[Command]
 }
 
+/**
+ * Points for problems 0 to 3 (2015-08-07 20:21):
+ *
+ * "Ei! " - [383, 0, 152, 326]
+ * "Ei!Ia! Ia!R'lyeh" - [0, 0, 316, 501]
+ */
 object NaivePowerPhrasesSolver extends Solver {
 
   // right now this is only for documentation
   val knownWords = List(
     "Ei!", // from statement
-    "Ia! Ia!", // (?) from problem 3 grid
-    "R'lyeh") // (?) from problem 5 grid
+    "Ia! Ia!", // from problem 3 grid
+    "R'lyeh", // from problem 5 grid
+    "Yuggoth") // from problem 7 grid
 
   def play(initialState: GameState) = {
     val commandsIter = Iterator.continually("Ei! ".toIterator).flatten
