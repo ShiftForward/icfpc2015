514c3efc3d97c46dfee663dde65e63f2c08dabef
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-08-07 19:41:38 +0100

    Validate transforms in GameState

--- src/main/scala/eu/shiftforward/icfpc2015/GameState.scala
+++ src/main/scala/eu/shiftforward/icfpc2015/GameState.scala
@@ -2,26 +2,46 @@ package eu.shiftforward.icfpc2015
 
 import eu.shiftforward.icfpc2015.model._
 
-case class GameState(grid: Grid, units: Iterator[CellUnit], currentUnitPos: Option[UnitPos], gameOver: Boolean) extends GridOperations {
+case class GameState(grid: Grid, units: Iterator[CellUnit], currentUnitPos: Option[UnitPos], gameOver: Boolean, prevStates: Set[UnitPos] = Set()) extends GridOperations {
+  def getNextUnitPos(nextGrid: Grid): GameState = {
+    if (!units.hasNext)
+      GameState(nextGrid, units, None, gameOver = true)
+    else {
+      initialPosition(units.next(), grid) match {
+        case None =>
+          GameState(nextGrid, units, None, gameOver = true)
+        case nextPos =>
+          GameState(nextGrid, units, nextPos, gameOver = false, prevStates = Set(nextPos.get))
+      }
+    }
+  }
+
   def nextState(move: Char): GameState = currentUnitPos match {
     case Some(pos) =>
       val command = Command(move)
       transform(pos, command, grid) match {
         case None =>
           val nextGrid = removeLines(lockCell(pos, grid))
-          GameState(nextGrid, units, initialPosition(units.next(), nextGrid), gameOver = false)
-        case nextPos => GameState(grid, units, nextPos, gameOver = false)
+          getNextUnitPos(nextGrid)
+        case nextPosOpt @ Some(nextPos) =>
+          if (validateTransform(nextPos, prevStates))
+            GameState(grid, units, nextPosOpt, gameOver = false, prevStates + nextPos)
+          else
+            GameState(grid, units, None, gameOver = true)
       }
     case None =>
-      GameState(grid, units, currentUnitPos, gameOver = true)
+      getNextUnitPos(grid)
   }
 
   def nextState(moves: String): GameState =
     if (moves.isEmpty) this
     else nextState(moves.head).nextState(moves.tail)
+
+  def start() = getNextUnitPos(grid)
 }
 
 object GameState extends GridOperations {
-  def apply(grid: Grid, units: Iterator[CellUnit]): GameState =
-    GameState(grid, units, initialPosition(units.next(), grid), gameOver = false)
+  def apply(grid: Grid, units: Iterator[CellUnit]): GameState = {
+    GameState(grid, units, None, gameOver = false).start()
+  }
 }
