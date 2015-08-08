dded1e2998135f389df029f0809caa5385093433
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-08-08 16:28:46 +0100

    Add a ReversePathFinder

--- src/main/scala/eu/shiftforward/icfpc2015/solver/PathFinder.scala
+++ src/main/scala/eu/shiftforward/icfpc2015/solver/PathFinder.scala
@@ -5,7 +5,42 @@ import eu.shiftforward.icfpc2015.GridOperations._
 import eu.shiftforward.icfpc2015.model._
 import scala.collection.mutable
 
-class PathFinder(grid: Grid, from: UnitPos) {
+trait PathFindingUtils {
+  def commandsToTest: Seq[Command]
+
+  def prev: mutable.Map[UnitPos, (UnitPos, Command, Int)]
+
+  def pathFindingLoop(from: UnitPos, to: UnitPos, grid: Grid) {
+    implicit val ordering = new Ordering[(Int, UnitPos)] {
+      def compare(p1: (Int, UnitPos), p2: (Int, UnitPos)) = p1._1 compare p2._1
+    }.reverse
+
+    val pq = mutable.PriorityQueue[(Int, UnitPos)]()
+    pq.enqueue((0, from))
+
+    def loop() {
+      if (!pq.isEmpty) {
+        val (_, currentPos) = pq.dequeue
+        val dist = if (currentPos == from) 0 else prev(currentPos)._3
+        if (currentPos != to) {
+          commandsToTest.foreach { command =>
+            transform(currentPos, command, grid).foreach { nextPos =>
+              if (!prev.contains(nextPos) && nextPos != from) {
+                prev.update(nextPos, (currentPos, command, dist + 1))
+                pq.enqueue((dist + 1 + nextPos.pos.distance(to.pos), nextPos))
+              }
+            }
+          }
+          loop()
+        }
+      }
+    }
+
+    loop()
+  }
+}
+
+class PathFinder(grid: Grid, from: UnitPos) extends PathFindingUtils {
   val commandsToTest = Seq(
     Command('p'),
     Command('b'),
@@ -32,28 +67,7 @@ class PathFinder(grid: Grid, from: UnitPos) {
     if (!fits(to, grid))
       None
     else {
-      val pq = mutable.PriorityQueue[(Int, UnitPos)]()
-      pq.enqueue((0, to))
-
-      def loop() {
-        if (!pq.isEmpty) {
-          val (_, currentPos) = pq.dequeue
-          val dist = if (currentPos == to) 0 else prev(currentPos)._3
-          if (currentPos != from) {
-            commandsToTest.foreach { command =>
-              transform(currentPos, command, grid).foreach { nextPos =>
-                if (!prev.contains(nextPos) && nextPos != to) {
-                  prev.update(nextPos, (currentPos, command, dist + 1))
-                  pq.enqueue((dist + 1 + nextPos.pos.distance(from.pos), nextPos))
-                }
-              }
-            }
-            loop()
-          }
-        }
-      }
-
-      loop()
+      pathFindingLoop(to, from, grid)
 
       prev.get(from) match {
         case Some(_) =>
@@ -72,6 +86,52 @@ class PathFinder(grid: Grid, from: UnitPos) {
 
           go(from)
           Some(c.map(invertedCommands).toList)
+
+        case None =>
+          None
+      }
+    }
+  }
+}
+
+class ReversePathFinder(grid: Grid, to: UnitPos) extends PathFindingUtils {
+  val commandsToTest = Seq(
+    Command('b'),
+    Command('p'),
+    Command('l'),
+    Command('a'),
+    Command('k'),
+    Command('d'))
+
+  implicit val ordering = new Ordering[(Int, UnitPos)] {
+    def compare(p1: (Int, UnitPos), p2: (Int, UnitPos)) = p1._1 compare p2._1
+  }.reverse
+
+  val prev = mutable.Map[UnitPos, (UnitPos, Command, Int)]()
+
+  def pathFrom(from: UnitPos): Option[List[Command]] = {
+    if (!fits(to, grid) || !fits(from, grid))
+      None
+    else {
+      pathFindingLoop(from, to, grid)
+
+      prev.get(to) match {
+        case Some(_) =>
+          val c = mutable.ListBuffer[Command]()
+          def go(p: UnitPos) {
+            if (p != from) {
+              prev.get(p) match {
+                case Some((pr, comm, _)) =>
+                  c += comm
+                  go(pr)
+
+                case None => // do nothing
+              }
+            }
+          }
+
+          go(to)
+          Some(c.toList.reverse)
 
         case None =>
           None
