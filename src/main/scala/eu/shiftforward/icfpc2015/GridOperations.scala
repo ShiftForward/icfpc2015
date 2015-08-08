8c737dee2e1bcc00e1b42f50790289a430a75d65
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-08-08 15:22:50 +0100

    Fix NE movement

--- src/main/scala/eu/shiftforward/icfpc2015/GridOperations.scala
+++ src/main/scala/eu/shiftforward/icfpc2015/GridOperations.scala
@@ -52,7 +52,7 @@ object GridOperations {
     case MoveSW => unitPos.copy(pos = unitPos.pos.copy(x = unitPos.pos.x - (if (unitPos.pos.y % 2 == 0) 1 else 0), y = unitPos.pos.y + 1))
     case MoveSE => unitPos.copy(pos = unitPos.pos.copy(x = unitPos.pos.x + (if (unitPos.pos.y % 2 == 0) 0 else 1), y = unitPos.pos.y + 1))
     case MoveNW => unitPos.copy(pos = unitPos.pos.copy(x = unitPos.pos.x - (if (unitPos.pos.y % 2 == 0) 1 else 0), y = unitPos.pos.y - 1))
-    case MoveNE => unitPos.copy(pos = unitPos.pos.copy(x = unitPos.pos.x - (if (unitPos.pos.y % 2 == 0) 0 else 1), y = unitPos.pos.y - 1))
+    case MoveNE => unitPos.copy(pos = unitPos.pos.copy(x = unitPos.pos.x + (if (unitPos.pos.y % 2 == 0) 0 else 1), y = unitPos.pos.y - 1))
     case RotateCW => unitPos.copy(unit = unitPos.unit.rotateCW)
     case RotateCCW => unitPos.copy(unit = unitPos.unit.rotateCCW)
   }
