3d581d72e1bfcfb355f986e51c643dcaa4819df6
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-08-09 23:05:28 +0100

    Update grid parameters on fill

--- src/main/scala/eu/shiftforward/icfpc2015/model/Input.scala
+++ src/main/scala/eu/shiftforward/icfpc2015/model/Input.scala
@@ -22,29 +22,12 @@ case class Grid(
     grid: Array[Array[Boolean]],
     lineCount: Array[Int],
     colCount: Array[Int],
-    heights: Array[Int]) {
-  lazy val (aggHeight, aggLow, bumpiness, holes, fullLines) = {
-    var maxHeight = heights(0)
-    var holesAcc = (heights(0) - colCount(0))
-    var minHeight = heights(0)
-    var bumpAcc = 0
-    var oldHeight = heights(0)
-    var col = 1
-
-    while (col < width) {
-      val newHeight = heights(col)
-      val newHoles = (heights(col) - colCount(col))
-      maxHeight = math.max(newHeight, maxHeight)
-      minHeight = math.min(newHeight, minHeight)
-      bumpAcc += math.abs(newHeight - oldHeight)
-      holesAcc += newHoles
-      oldHeight = newHeight
-      col += 1
-    }
-
-    (maxHeight, minHeight, bumpAcc, holesAcc, lineCount.count(_ == width))
-  }
-
+    heights: Array[Int],
+    aggHeight: Int,
+    aggLow: Int,
+    bumpiness: Int,
+    holes: Int,
+    fullLines: Int) {
   lazy val highLow = aggHeight - aggLow
 
   private[this] def column(c: Int): Array[Boolean] = grid.map(_(c))
@@ -55,21 +38,72 @@ case class Grid(
     val newLineCount = lineCount.clone()
     val newColCount = colCount.clone()
     val newHeights = heights.clone()
+    var newAggHeight = aggHeight
+    var newAggLow = aggLow
+    var newBumpiness = bumpiness
+    var newHoles = holes
+    var newFullLines = fullLines
+    var reviewAggLow = false
+
     val cloned = mutable.BitSet()
+
     cells.foreach { cell =>
       if (!cloned(cell.row)) {
         newGrid(cell.row) = newGrid(cell.row).clone()
         cloned += cell.row
       }
-      newGrid(cell.row)(cell.col) = true
-      if (!grid(cell.row)(cell.col)) {
+
+      if (!newGrid(cell.row)(cell.col)) {
+        newGrid(cell.row)(cell.col) = true
+
+        val prevColCount = newColCount(cell.col)
+        val prevLineCount = newLineCount(cell.row)
+        val prevHeight = newHeights(cell.col)
+
         newLineCount(cell.row) += 1
         newColCount(cell.col) += 1
+
+        if (prevLineCount != width && newLineCount(cell.row) == width)
+          newFullLines += 1
+
+        if ((height - cell.row) > newHeights(cell.col)) {
+          newHeights(cell.col) = height - cell.row
+
+          if (prevHeight == newAggLow) {
+            reviewAggLow = true
+          }
+
+          if (newHeights(cell.col) > newAggHeight)
+            newAggHeight = newHeights(cell.col)
+
+          if (cell.col > 0) {
+            newBumpiness -= math.abs(prevHeight - newHeights(cell.col - 1))
+            newBumpiness += math.abs(newHeights(cell.col) - newHeights(cell.col - 1))
+          }
+          if (cell.col < width - 1) {
+            newBumpiness -= math.abs(prevHeight - newHeights(cell.col + 1))
+            newBumpiness += math.abs(newHeights(cell.col) - newHeights(cell.col + 1))
+          }
+        }
+
+        newHoles -= (prevHeight - prevColCount)
+        newHoles += (newHeights(cell.col) - newColCount(cell.col))
       }
-      if ((height - cell.row) > newHeights(cell.col))
-        newHeights(cell.col) = height - cell.row
     }
-    copy(grid = newGrid, lineCount = newLineCount, colCount = newColCount, heights = newHeights)
+
+    if (reviewAggLow)
+      newAggLow = newHeights.min
+
+    copy(
+      grid = newGrid,
+      lineCount = newLineCount,
+      colCount = newColCount,
+      heights = newHeights,
+      aggHeight = newAggHeight,
+      aggLow = newAggLow,
+      bumpiness = newBumpiness,
+      holes = newHoles,
+      fullLines = newFullLines)
   }
 }
 
@@ -88,6 +122,31 @@ object Grid {
         val h = grid.indexWhere(_(col))
         if (h < 0) 0 else height - h
       }.toArray)
+  def apply(width: Int, height: Int, grid: Array[Array[Boolean]], lineCount: Array[Int], colCount: Array[Int], heights: Array[Int]): Grid = {
+    val (aggHeight, aggLow, bumpiness, holes, fullLines) = {
+      var maxHeight = heights(0)
+      var holesAcc = (heights(0) - colCount(0))
+      var minHeight = heights(0)
+      var bumpAcc = 0
+      var oldHeight = heights(0)
+      var col = 1
+
+      while (col < width) {
+        val newHeight = heights(col)
+        val newHoles = (heights(col) - colCount(col))
+        maxHeight = math.max(newHeight, maxHeight)
+        minHeight = math.min(newHeight, minHeight)
+        bumpAcc += math.abs(newHeight - oldHeight)
+        holesAcc += newHoles
+        oldHeight = newHeight
+        col += 1
+      }
+
+      (maxHeight, minHeight, bumpAcc, holesAcc, lineCount.count(_ == width))
+    }
+
+    Grid(width, height, grid, lineCount, colCount, heights, aggHeight, aggLow, bumpiness, holes, fullLines)
+  }
 }
 
 final case class Cube(x: Int, y: Int, z: Int) {
